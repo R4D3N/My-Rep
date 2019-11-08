3858683dc73c4c970192d42dc300489c1e8738c3
R4D3N <raden-@outlook.com>
2019-11-08 09:33:08 +0700

    Update fizzbuzz.cpp

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -2,13 +2,14 @@
 
 int main()
 {
+    using std::cout;
     int i;
-    for (i = 0; i <= 100; i++)
+    for (i = 1; i <= 100; i++)
     {
-        if (i % 15 == 0) cout << "FizzBuzz" << endl;
-        if (i % 5 == 0) cout << "Buzz" << endl;
-        if (i % 3 == 0) cout << "Fizz" << endl;
-        else cout << i << endl;
+        if (i % 15 == 0) cout << "FizzBuzz\n";
+        else if (i % 5 == 0) cout << "Buzz\n";
+        else if (i % 3 == 0) cout << "Fizz\n";
+        else cout << i << "\n";
     }
     return 0;
 }
